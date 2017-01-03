de20d7f4229e401eba9ac14852ad12b4fce91179
kongnet <kongnet@rui10.com>
2017-01-03 12:36:45 +0800

    增加tpl覆盖2

--- tests/unit_checkData.js
+++ tests/unit_checkData.js
@@ -686,6 +686,7 @@ describe('模板引擎单元测试', function() {
 	it('tpl', function() {
 		$.tpl.config({open:'{{',close:'}}'});
 		assert.strictEqual('<div></div>', $.tpl('<{{d.tag}}></{{d.tag}}>').render({tag:'div'}));
+		assert.strictEqual('<di&amp;v></di&v>', $.tpl('<{{=d.tag}}></{{d.tag}}>').render({tag:'di&v'}));//=转义html标记
 		assert.strictEqual(true, $.tpl('<{{# 1+1 }></{{d.tag}}>').render({tag:'div'}).indexOf('Laytpl Error')>=0);//模板结构不对
 	});
 });
