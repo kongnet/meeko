6684edd9c3b963a3f1d2ca728660f2e85df5d644
kongnet <kongnet@rui10.com>
2017-01-13 19:46:58 +0800

    静态规则修改

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 [![Codacy Badge](https://api.codacy.com/project/badge/Grade/fb7f44bf54b742ec97db7c17f49ceb4c)](https://www.codacy.com/app/9601698/meeko?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=kongnet/meeko&amp;utm_campaign=Badge_Grade)
 
 [![NPM](https://nodei.co/npm/meeko.png?downloads=true&stars=true)](https://nodei.co/npm/meeko/)
+
+[![Standard - JavaScript Style Guide](https://cdn.rawgit.com/feross/standard/master/badge.svg)](https://github.com/kongnet/meeko)
--- index.js
+++ index.js
@@ -1,51 +1,51 @@
-'use strict';
-const _s = require('./lib/string');
-ext(String.prototype, _s);
-const _d = require('./lib/date');
-ext(Date.prototype, _d);
+'use strict'
+const _s = require('./lib/string')
+ext(String.prototype, _s)
+const _d = require('./lib/date')
+ext(Date.prototype, _d)
 let option = {
   logTime: true
-};
+}
 
-function ext(a, b) {
+function ext (a, b) {
   if (a && b) {
     for (let c in b) {
       if (b.hasOwnProperty(c)) {
-        a[c] = b[c];
+        a[c] = b[c]
       }
     }
-    return a;
+    return a
   }
-  return null;
+  return null
 }
 
-Array.prototype.copy = function() {
-  return [].concat(this);
-};
+Array.prototype.copy = function () {
+  return [].concat(this)
+}
 
-Number.prototype.round = function(p) {
-  p = Math.pow(10, p || 0);
-  return Math.round(this * p) / p;
-};
-Number.prototype.fillStr = String.prototype.fillStr;
-Date.prototype.fillStr = String.prototype.fillStr;
-Buffer.prototype.contact = function(b) {
+Number.prototype.round = function (p) {
+  p = Math.pow(10, p || 0)
+  return Math.round(this * p) / p
+}
+Number.prototype.fillStr = String.prototype.fillStr
+Date.prototype.fillStr = String.prototype.fillStr
+Buffer.prototype.contact = function (b) {
   /*
   utf8 有bom头
   EF BB BF [239 187 191]
   */
-  let buf = new Buffer(this.length + b.length);
-  this.copy(buf, 0, 0, this.length);
-  b.copy(buf, this.length, 0, b.length);
-  return buf;
-};
+  let buf = new Buffer(this.length + b.length)
+  this.copy(buf, 0, 0, this.length)
+  b.copy(buf, this.length, 0, b.length)
+  return buf
+}
 let c = {
   /*
   http://stanislavs.org/helppc/ansi_codes.html
   */
   cls: '\x1b[0;0;H\x1b[0J',
-  xy(x, y) {
-    return `\x1b[${y};${x};H`;
+  xy (x, y) {
+    return `\x1b[${y};${x};H`
   },
   none: '\x1b[m',
   black: '\x1b[30m',
@@ -63,49 +63,48 @@ let c = {
   lmagenta: '\x1b[35m',
   lcyan: '\x1b[36m',
   lwhite: '\x1b[37m'
-};
+}
 
-let getStackTrace = function() {
-  let obj = {};
-  Error.captureStackTrace(obj, getStackTrace);
-  return obj.stack;
-};
+let getStackTrace = function () {
+  let obj = {}
+  Error.captureStackTrace(obj, getStackTrace)
+  return obj.stack
+}
 
-let re = /\\(.+)\.js:(\d+:\d+)/g;
-let trace = console;
-let log = function(...args) {
-  getStackTrace().split('\n')[2].match(re);
-  let s = c.none + ' [' + c.lgreen + RegExp.$1.split('\\').pop() + ':' + RegExp.$2 + ' ' + new Date().date2Str().replaceAll('-', '') + c.none + ']';
-  let str = '';
+let re = /\\(.+)\.js:(\d+:\d+)/g
+let trace = console
+let log = function (...args) {
+  getStackTrace().split('\n')[2].match(re)
+  let s = c.none + ' [' + c.lgreen + RegExp.$1.split('\\').pop() + ':' + RegExp.$2 + ' ' + new Date().date2Str().replaceAll('-', '') + c.none + ']'
+  let str = ''
   for (let i = 0; i < args.length; i++) {
     if (typeof args[i] === 'object') {
-      str = str + JSON.stringify(args[i]) + ' ';
+      str = str + JSON.stringify(args[i]) + ' '
     } else {
-      str = str + args[i] + ' ';
+      str = str + args[i] + ' '
     }
   }
-  trace.log(str + (option.logTime ? s : ''));
-  return 1;
-};
-let err = function(...args) {
-  getStackTrace().split('\n')[2].match(re);
-  let s = c.none + ' [' + c.lred + RegExp.$1.split('\\').pop() + ':' + RegExp.$2 + ' ' + new Date().date2Str().replaceAll('-', '') + c.none + ']';
-  let str = '';
+  trace.log(str + (option.logTime ? s : ''))
+  return 1
+}
+let err = function (...args) {
+  getStackTrace().split('\n')[2].match(re)
+  let s = c.none + ' [' + c.lred + RegExp.$1.split('\\').pop() + ':' + RegExp.$2 + ' ' + new Date().date2Str().replaceAll('-', '') + c.none + ']'
+  let str = ''
   for (let i = 0; i < args.length; i++) {
     if (typeof args[i] === 'object') {
-      str = str + JSON.stringify(args[i]) + ' ';
+      str = str + JSON.stringify(args[i]) + ' '
     } else {
-      str = str + args[i] + ' ';
+      str = str + args[i] + ' '
     }
   }
-  trace.log(str + (option.logTime ? s : ''));
-  return 1;
-};
+  trace.log(str + (option.logTime ? s : ''))
+  return 1
+}
 
-
-const tools = require('./lib/tools');
-const fake = require('./lib/fake');
-const tpl = require('./lib/tpl');
+const tools = require('./lib/tools')
+const fake = require('./lib/fake')
+const tpl = require('./lib/tpl')
 
 module.exports = {
   option,
@@ -116,4 +115,4 @@ module.exports = {
   tools,
   fake,
   tpl
-};
+}
