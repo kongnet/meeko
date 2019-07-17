a0267c660ace1c4745540dd7230ef8020292a177
kongnet <9601698@qq.com>
2019-07-17 15:25:22 +0800

    :art:

--- tests/6_math.js
+++ tests/6_math.js
@@ -2,13 +2,13 @@
 /* global it */
 'use strict'
 var $ = require('../index')
-let assert = require('assert')
-let assertLog = function () {
+const assert = require('assert')
+const assertLog = function () {
   global.assertCount++
   return assert.strictEqual(...arguments)
 }
 describe('Math扩展函数的单元测试', () => {
-  let a = [2, 1, 8.1, 3, 4, 5.1, 6.7]
+  const a = [2, 1, 8.1, 3, 4, 5.1, 6.7]
   it('sum', () => {
     assertLog($.math.sum(a).toFixed(1), '29.9')
   })
@@ -78,27 +78,27 @@ describe('Math扩展函数的单元测试', () => {
   $.math.powerFitting()
   $.math.pearson()
   */
-  let rst = $.math.linearFitting([1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 4, 3, 6, 9, 3, 5, 2, 5])
+  const rst = $.math.linearFitting([1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 4, 3, 6, 9, 3, 5, 2, 5])
   it(`linearFitting ${rst.f}`, () =>
     assertLog(JSON.stringify(rst), `{"a":0.18333333333333332,"b":3.305555555555556,"r":0.04426829268292683,"f":"y=0.1833*x+3.3056 R^2=0.0443"}`)
   )
 
-  let rst1 = $.math.exponentFitting([1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 4, 3, 6, 9, 3, 5, 2, 5])
+  const rst1 = $.math.exponentFitting([1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 4, 3, 6, 9, 3, 5, 2, 5])
   it(`exponentFitting ${rst1.f}`, () =>
     assertLog(JSON.stringify(rst1), `{"a":2.4241733882720133,"b":0.07811356958381002,"r":0.10831424034090119,"f":"y=2.4242*e^(0.0781*x) R^2=0.1083"}`)
   )
 
-  let rst2 = $.math.lnFitting([1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 4, 3, 6, 9, 3, 5, 2, 5])
+  const rst2 = $.math.lnFitting([1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 4, 3, 6, 9, 3, 5, 2, 5])
   it(`lnFitting ${rst2.f}`, () =>
     assertLog(JSON.stringify(rst2), `{"a":1.224733441070464,"b":2.480130419814377,"r":0.1362718140723164,"f":"y=1.2247*ln(x)+2.4801 R^2=0.1363"}`)
   )
 
-  let rst3 = $.math.powerFitting([1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 4, 3, 6, 9, 3, 5, 2, 5])
+  const rst3 = $.math.powerFitting([1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 4, 3, 6, 9, 3, 5, 2, 5])
   it(`powerFitting ${rst3.f}`, () =>
     assertLog(JSON.stringify(rst3), `{"a":1.8454140471460965,"b":0.46635474401809107,"r":0.26630441651302567,"f":"y=1.8454*x^0.4664 R^2=0.2663"}`)
   )
-  let rst4 = $.math.pearson([56, 97, 76, 59, 86, 62, 32, 69, 75, 79, 36, 70, 48, 57, 86, 88, 92, 85, 75, 48], [8, 45, 35, 12, 37, 24, 5, 21, 36, 32, 10, 27, 15, 19, 41, 50, 39, 42, 35, 17])
-  let rst5 = $.math.pearson([2.5, 3.5, 3.0, 3.5, 2.5, 3.0], [3.0, 3.5, 1.5, 5.0, 3.5, 3.0])
+  const rst4 = $.math.pearson([56, 97, 76, 59, 86, 62, 32, 69, 75, 79, 36, 70, 48, 57, 86, 88, 92, 85, 75, 48], [8, 45, 35, 12, 37, 24, 5, 21, 36, 32, 10, 27, 15, 19, 41, 50, 39, 42, 35, 17])
+  const rst5 = $.math.pearson([2.5, 3.5, 3.0, 3.5, 2.5, 3.0], [3.0, 3.5, 1.5, 5.0, 3.5, 3.0])
   it(`pearson相关指数`, () => {
     assertLog(rst4.toFixed(3), '0.931')
     assertLog(rst5.toFixed(3), '0.396')
@@ -106,7 +106,7 @@ describe('Math扩展函数的单元测试', () => {
 })
 
 describe('Math偏度和3个峰度函数单元测试', () => {
-  let a = [53, 61, 49, 66, 78, 47]
+  const a = [53, 61, 49, 66, 78, 47]
   it('skew 偏度 pandas模块 0.7826325504212567', () =>
     assertLog($.math.skew(a).toFixed(6), '0.782633')
   )
