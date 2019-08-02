4d40e8ac4febe578815625773ee767575b018533
kongnet <9601698@qq.com>
2019-08-02 16:23:55 +0800

    :sparkles: 增加唯美短语，增加多种分布函数

--- tests/7_mock.js
+++ tests/7_mock.js
@@ -6,12 +6,12 @@ const Mock = require('mockjs')
 
 Mock.Random.extend($.Mock)
 
-let data = Mock.mock({
+const data = Mock.mock({
   // 属性 list 的值是一个数组，其中含有 1 到 10 个元素
   'list|10-10': [{
     // 属性 id 是一个自增数，起始值为 1，每次增 1
     'id|+1': 100,
-    'name': `@genName`,
+    name: `@genName`,
     str: `@genData('abcdefghijklmnopqrstuvwxyz',10)`,
     datetime: `@genDatetime('2016-1-1', '2016-2-2')`,
     card: `@genCard`,
@@ -26,6 +26,7 @@ let data = Mock.mock({
     sentence20: `@genText(20)`,
     sentence10: `@genText(10)`,
     sentence0: `@genText`,
+    sentence1: `@genBeautyText`,
     constellation: `@genConstellation`,
     bool: `@genBool`,
     genEnum1: `@genEnum(['5',6,7])`, // NOTICE key不能和函数名一样
