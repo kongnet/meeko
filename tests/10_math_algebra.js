172e9704ad8525a4a1f81e722937c29de5247cc0
kongnet <9601698@qq.com>
2019-11-11 17:59:42 +0800

    :sparkles: 增加gcd lcm 因式分解函数

--- tests/10_math_algebra.js
+++ tests/10_math_algebra.js
@@ -3,7 +3,7 @@
 'use strict'
 const $ = require('../index')
 const assert = require('assert')
-require('chai').should()
+
 const assertLog = function () {
   global.assertCount++
   return assert.strictEqual(...arguments)
@@ -18,11 +18,25 @@ describe('代数函数测试', function () {
    -1, -3, 5, 7, 13
     9, -5, 3, 6, 26`
       const rst = [3, 5, 2, 3]
-      const r = $.math.gaussian($.file.csv2Arr(matrixStr, ',', 1, (v) => +v.trim()))
+      const r = $.math
+        .gaussian($.file.csv2Arr(matrixStr, ',', 1, v => +v.trim()))
         .map(x => +x.toFixed(10))
         .equals(rst)
       assertLog(r, true)
       console.log(matrixStr, '\n', '=>', rst)
+    })
+  })
+  describe('最大公约数，最小公倍数', function () {
+    it('gcdlcm因式分解', function () {
+      assertLog($.math.gcd(10, 12), 2)
+      assertLog($.math.lcm(10, 12), 60)
+      assertLog(
+        $.math.primeFactor(12345678912345).join('*'),
+        '3*5*43*2371*8072791'
+      )
+      assertLog($.math.primeFactor(1).join('*'), '1')
+      assertLog($.math.primeFactor(0).join('*'), '0')
+      assertLog($.math.primeFactor(2).join('*'), '2')
     })
   })
 })
