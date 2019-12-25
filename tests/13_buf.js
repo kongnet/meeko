a459260e78e24cdd555a0135d053d675dc125d35
kongnet <9601698@qq.com>
2019-12-25 17:14:42 +0800

    :art: 修改buf方法名称

--- tests/13_buf.js
+++ tests/13_buf.js
@@ -9,14 +9,14 @@ const assertLog = function () {
 }
 
 let rr = Buffer.from([0x20, 0x0, 0x59, 0x67, 0xff, 0x1, 0x0, 0x11, 0x45, 0x0])
-const arr = $.buf.splitBuffer(rr, $.buf.zero)
+const arr = $.buf.split(rr, $.buf.zero)
 
 describe('buffer扩展', function () {
   it('buffer按字符split', function () {
     assertLog(arr[1][2], 255)
   })
   it('bufferArray按字符join', function () {
-    const buf = $.buf.joinBuffer(arr, $.buf.zero)
+    const buf = $.buf.join(arr, $.buf.zero)
     assertLog(buf[7], 0x11)
   })
 })
