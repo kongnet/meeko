7c435319ab4814e01767b6ef863c4f3d27e52b67
sky <sky@skydeMacBook-Pro.local>
2022-04-15 13:47:14 +0800

    :wrench:

--- sample/鸢尾花PCA.js
+++ sample/鸢尾花PCA.js
@@ -1,6 +1,6 @@
 const $ = require('../index')
 const PCA = $.ml.Pca
-const movieDim = [
+let movieDim = [
   [5.1, 3.5, 1.4, 0.2],
   [4.9, 3.0, 1.4, 0.2],
   [4.7, 3.2, 1.3, 0.2],
@@ -152,7 +152,7 @@ const movieDim = [
   [6.2, 3.4, 5.4, 2.3],
   [5.9, 3.0, 5.1, 1.8]
 ]
-movieTag = [
+let movieTag = [
   'Iris-setosa',
   'Iris-setosa',
   'Iris-setosa',
